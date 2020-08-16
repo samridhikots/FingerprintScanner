d94bca7271acc877055a5144a7b7f1e452fc57cc
Samridhi <samridhikots@gmail.com>
2020-08-16 19:35:59 +0530

    Initial commit

---
+++ app/src/main/java/com/samridhi/fingerprint/scanner/MainActivity.java
@@ -0,0 +1,155 @@
+package com.samridhi.fingerprint.scanner;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.KeyguardManager;
+import android.content.pm.PackageManager;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.Build;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyPermanentlyInvalidatedException;
+import android.security.keystore.KeyProperties;
+import androidx.core.content.ContextCompat;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private TextView mHeadingLabel;
+    private ImageView mFingerprintImage;
+    private TextView textLabel;
+
+    private FingerprintManager fingerprintManager;
+    private KeyguardManager keyguardManager;
+
+    private KeyStore keyStore;
+    private Cipher cipher;
+    private String KEY_NAME = "AndroidKey";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mHeadingLabel = (TextView) findViewById(R.id.headingLabel);
+        mFingerprintImage = (ImageView) findViewById(R.id.fingerprintImage);
+        textLabel = (TextView) findViewById(R.id.textViewLabel);
+
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {        //atleast Marshmellow
+
+            fingerprintManager = (FingerprintManager) getSystemService(FINGERPRINT_SERVICE);
+            keyguardManager = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
+
+            if(!fingerprintManager.isHardwareDetected()){           //check for fingerprintScanner 
+
+               textLabel.setText("Fingerprint Scanner not detected in Device");
+
+            } else if (ContextCompat.checkSelfPermission(this, Manifest.permission.USE_BIOMETRIC) != PackageManager.PERMISSION_GRANTED){            //checks permission of using the fingerprint
+
+                textLabel.setText("Permission not granted to use Fingerprint Scanner");
+
+            } else if (!keyguardManager.isKeyguardSecure()){                // checks if atleast having 1 lock(pattern, or anything like that)
+
+                textLabel.setText("Add Lock to your Phone in Settings");
+
+            } else if (!fingerprintManager.hasEnrolledFingerprints()){          //checks if it has atleast 1 fingerprint or not
+
+                textLabel.setText("You should add atleast 1 Fingerprint to use this Feature");
+
+            } else {
+
+                textLabel.setText("Place your Finger on Scanner to Access the App.");
+
+                generateKey();
+
+                if (cipherInit()){
+
+                    FingerprintManager.CryptoObject cryptoObject = new FingerprintManager.CryptoObject(cipher);
+                    FingerprintHandler fingerprintHandler = new FingerprintHandler(this);
+                    fingerprintHandler.startAuth(fingerprintManager, cryptoObject);
+
+                }
+            }
+
+        }
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void generateKey() {
+
+        try {
+
+            keyStore = KeyStore.getInstance("AndroidKeyStore");
+            KeyGenerator keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore");
+
+            keyStore.load(null);
+            keyGenerator.init(new
+                    KeyGenParameterSpec.Builder(KEY_NAME,
+                    KeyProperties.PURPOSE_ENCRYPT |
+                            KeyProperties.PURPOSE_DECRYPT)
+                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                    .setUserAuthenticationRequired(true)
+                    .setEncryptionPaddings(
+                            KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                    .build());
+            keyGenerator.generateKey();
+
+        } catch (KeyStoreException | IOException | CertificateException
+                | NoSuchAlgorithmException | InvalidAlgorithmParameterException
+                | NoSuchProviderException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    public boolean cipherInit() {
+        try {
+            cipher = Cipher.getInstance(KeyProperties.KEY_ALGORITHM_AES + "/" + KeyProperties.BLOCK_MODE_CBC + "/" + KeyProperties.ENCRYPTION_PADDING_PKCS7);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            throw new RuntimeException("Failed to get Cipher", e);
+        }
+
+
+        try {
+
+            keyStore.load(null);
+
+            SecretKey key = (SecretKey) keyStore.getKey(KEY_NAME,
+                    null);
+
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+
+            return true;
+
+        } catch (KeyPermanentlyInvalidatedException e) {
+            return false;
+        } catch (KeyStoreException | CertificateException | UnrecoverableKeyException | IOException | NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new RuntimeException("Failed to init Cipher", e);
+        }
+
+    }
+
+}
